186ddba5848927e7b68af865f97fa80f19fb3810
Abhishek Srikanth <abhisrik99@outlook.com>
2024-02-12 22:04:38 -0500

    refactor: navbar height

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar({ activeButton, onButtonClick }) {
     const buttonTexts = ['Home', 'Skills', 'Projects', 'Experience', 'Recommendations', 'Awards', 'Education']
 
     return (
-        <div className="h-1/6 flex">
+        <div className="h-[8.33%] flex py-5">
             <nav className="space-x-3 items-center text-lg m-auto px-4">
                 {
                     buttonTexts.map((buttonText, i) => <NavBarButton
